c976a1eed97ad4f32c759a956f872ecf33fb25a6
MrKomodoDragon <svrchn921@outlook.com>
2021-04-09 17:20:20 -0700

    cargo format

--- c_to_f/src/main.rs
+++ c_to_f/src/main.rs
@@ -5,12 +5,16 @@ fn main() {
     let mut input = String::new();
     println!("What would number would you like to convert to Farenheit?");
     //read the input
-    io::stdin().read_line(&mut input).expect("Not a valid string");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Not a valid string");
     //parse the input
     let _input_num: f64 = input.trim().parse().expect("Not a valid float");
     //actual conversion
-    let number: f64 = _input_num * 9.0/5.0 + 32.0; 
+    let number: f64 = _input_num * 9.0 / 5.0 + 32.0;
     //printing the output
-    println!("{} degrees celsius is equal to {} degrees farenheit", _input_num, number);
-
+    println!(
+        "{} degrees celsius is equal to {} degrees farenheit",
+        _input_num, number
+    );
 }
